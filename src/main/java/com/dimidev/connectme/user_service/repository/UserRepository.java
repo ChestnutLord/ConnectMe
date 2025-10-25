52c6cc3a5db824ac39b6dfe0136308d3abc77cc4
ChestnutLord <134998927+ChestnutLord@users.noreply.github.com>
2025-10-25 22:03:08 +0300

    refactor: remove monolith components in preparation for microservices

--- src/main/java/com/dimidev/connectme/user_service/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dimidev.connectme.user_service.repository;
-
-import com.dimidev.connectme.user_service.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
