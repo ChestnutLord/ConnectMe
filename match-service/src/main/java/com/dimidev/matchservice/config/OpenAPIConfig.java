04bc6b835495bb6c1b12ad57788959c6b6e57d81
ChestnutLord <134998927+ChestnutLord@users.noreply.github.com>
2026-01-19 23:17:03 +0300

    fix(match-service): fix OpenAPI server url for gateway integration

--- match-service/src/main/java/com/dimidev/matchservice/config/OpenAPIConfig.java
+++ match-service/src/main/java/com/dimidev/matchservice/config/OpenAPIConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
@@ -17,6 +20,9 @@ public class OpenAPIConfig {
                         .description("This is REST API for Match Service")
                         .version("v0.0.1")
                         .license(new License().name("Apache 2.0")))
+                .servers(List.of(
+                        new Server().url("/")
+                ))
                 .externalDocs(new ExternalDocumentation()
                         .description("You can refer to the Match Service Wiki Documentation")
                         .url("https://match-service-dummy-url.com/docs"));
