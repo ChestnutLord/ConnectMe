4cb7e62d1abf639f4512d8b031b1f8400ebd03f2
ChestnutLord <134998927+ChestnutLord@users.noreply.github.com>
2026-01-19 23:03:48 +0300

    refactor(match-service): replace Feign with RestClient

--- match-service/src/main/java/com/dimidev/matchservice/client/UserClient.java
+++ match-service/src/main/java/com/dimidev/matchservice/client/UserClient.java
@@ -1,13 +1,11 @@
 package com.dimidev.matchservice.client;
 
 import com.dimidev.matchservice.dto.user.UserDto;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "user-service", url = "http://user-service:8080")
 public interface UserClient {
 
-    @GetMapping(path="api/v1/users/{id}")
+    @GetExchange("api/v1/users/{id}")
     UserDto getById(@PathVariable Long id);
 }
