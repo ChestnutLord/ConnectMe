ae366135dcdb672df7084c27652a40897f2d5433
ChestnutLord <134998927+ChestnutLord@users.noreply.github.com>
2026-01-19 23:16:18 +0300

    fix(gateway-service): fix route matching order for user and match services

--- gateway-service/src/main/java/com/dimidev/gatewayservice/routes/Routes.java
+++ gateway-service/src/main/java/com/dimidev/gatewayservice/routes/Routes.java
@@ -4,45 +4,20 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.rewritePath;
 
 @Configuration
 public class Routes {
 
-    // USER-SERVICE
-
-    @Bean
-    public RouterFunction<ServerResponse> userServiceRoutes() {
-        return GatewayRouterFunctions.route("user_service")
-                .route(
-                        RequestPredicates.path("/api/v1/users/**"),
-                        HandlerFunctions.http("http://user-service:8080")
-                )
-                .build();
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> userServiceSwaggerRoute() {
-        return GatewayRouterFunctions.route("user_service_swagger")
-                .route(
-                        RequestPredicates.path("/aggregate/user-service/v3/api-docs"),
-                        HandlerFunctions.http("http://user-service:8080")
-                )
-                .filter(
-                        rewritePath(
-                                "/aggregate/user-service/v3/api-docs",
-                                "/v3/api-docs"
-                        )
-                )
-                .build();
-    }
-
     // MATCH-SERVICE
 
     @Bean
+    @Order(10)
     public RouterFunction<ServerResponse> matchEsteemRoutes() {
         return GatewayRouterFunctions.route("match_service_esteems")
                 .route(
@@ -53,6 +28,7 @@ public class Routes {
     }
 
     @Bean
+    @Order(20)
     public RouterFunction<ServerResponse> matchMatchesRoutes() {
         return GatewayRouterFunctions.route("match_service_matches")
                 .route(
@@ -63,6 +39,7 @@ public class Routes {
     }
 
     @Bean
+    @Order(30)
     public RouterFunction<ServerResponse> matchServiceSwaggerRoute() {
         return GatewayRouterFunctions.route("match_service_swagger")
                 .route(
@@ -78,4 +55,33 @@ public class Routes {
                 .build();
     }
 
+    // USER-SERVICE
+
+    @Bean
+    @Order(100)
+    public RouterFunction<ServerResponse> userServiceRoutes() {
+        return GatewayRouterFunctions.route("user_service")
+                .route(
+                        RequestPredicates.path("/api/v1/users/**"),
+                        HandlerFunctions.http("http://user-service:8080")
+                )
+                .build();
+    }
+
+    @Bean
+    @Order(110)
+    public RouterFunction<ServerResponse> userServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("user_service_swagger")
+                .route(
+                        RequestPredicates.path("/aggregate/user-service/v3/api-docs"),
+                        HandlerFunctions.http("http://user-service:8080")
+                )
+                .filter(
+                        rewritePath(
+                                "/aggregate/user-service/v3/api-docs",
+                                "/v3/api-docs"
+                        )
+                )
+                .build();
+    }
 }
