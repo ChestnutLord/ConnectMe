a340cdd72787a34d475f00726b182050f0e91917
ChestnutLord <134998927+ChestnutLord@users.noreply.github.com>
2026-01-20 23:11:46 +0300

    feat(gateway-service): add Resilience4j circuit breakers with fallback routing

--- gateway-service/src/main/java/com/dimidev/gatewayservice/routes/Routes.java
+++ gateway-service/src/main/java/com/dimidev/gatewayservice/routes/Routes.java
@@ -1,15 +1,19 @@
 package com.dimidev.gatewayservice.routes;
 
-import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import java.net.URI;
+
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.rewritePath;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
 @Configuration
 public class Routes {
@@ -19,33 +23,37 @@ public class Routes {
     @Bean
     @Order(10)
     public RouterFunction<ServerResponse> matchEsteemRoutes() {
-        return GatewayRouterFunctions.route("match_service_esteems")
+        return route("match_service_esteems")
                 .route(
                         RequestPredicates.path("/api/v1/users/*/esteems/**"),
                         HandlerFunctions.http("http://match-service:8080")
                 )
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("matchServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
     @Order(20)
     public RouterFunction<ServerResponse> matchMatchesRoutes() {
-        return GatewayRouterFunctions.route("match_service_matches")
+        return route("match_service_matches")
                 .route(
                         RequestPredicates.path("/api/v1/users/*/matches/**"),
                         HandlerFunctions.http("http://match-service:8080")
-                )
+                ).filter(CircuitBreakerFilterFunctions.circuitBreaker("matchServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
     @Order(30)
     public RouterFunction<ServerResponse> matchServiceSwaggerRoute() {
-        return GatewayRouterFunctions.route("match_service_swagger")
+        return route("match_service_swagger")
                 .route(
                         RequestPredicates.path("/aggregate/match-service/v3/api-docs"),
                         HandlerFunctions.http("http://match-service:8080")
-                )
+                ).filter(CircuitBreakerFilterFunctions.circuitBreaker("matchServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .filter(
                         rewritePath(
                                 "/aggregate/match-service/v3/api-docs",
@@ -60,28 +68,40 @@ public class Routes {
     @Bean
     @Order(100)
     public RouterFunction<ServerResponse> userServiceRoutes() {
-        return GatewayRouterFunctions.route("user_service")
+        return route("user_service")
                 .route(
                         RequestPredicates.path("/api/v1/users/**"),
                         HandlerFunctions.http("http://user-service:8080")
-                )
+                ).filter(CircuitBreakerFilterFunctions.circuitBreaker("userServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
     @Order(110)
     public RouterFunction<ServerResponse> userServiceSwaggerRoute() {
-        return GatewayRouterFunctions.route("user_service_swagger")
+        return route("user_service_swagger")
                 .route(
                         RequestPredicates.path("/aggregate/user-service/v3/api-docs"),
                         HandlerFunctions.http("http://user-service:8080")
-                )
+                ).filter(CircuitBreakerFilterFunctions.circuitBreaker("userServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .filter(
                         rewritePath(
                                 "/aggregate/user-service/v3/api-docs",
                                 "/v3/api-docs"
                         )
                 )
+                .build();
+    }
+
+    // fallbackRoute
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service Unavailable, please try again later"))
                 .build();
     }
 }
